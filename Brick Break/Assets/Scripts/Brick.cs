30ef827c20c7a160da07a74d6532e726f89ca0e5
Pedro Nogueira <cmcg89034@gmail.com>
2025-11-03 17:55:06 -0300

    ♻️refac: turned bricks into prefabs with hp!

---
+++ Brick Break/Assets/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    [SerializeField]
+    private float totalBrickHealth = 5;
+
+    private float currBrickHealth;
+
+    private void Start() {
+        this.currBrickHealth = this.totalBrickHealth;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        this.currBrickHealth--;
+        if (this.currBrickHealth <= 0) {
+            Destroy(this.gameObject);
+        } else {
+            float gbValue = (this.currBrickHealth - 1) / this.totalBrickHealth;
+            Transform visual = this.transform.Find("Visual");
+            if (visual != null) {
+                Debug.Log("Found it!");
+                visual.GetComponent<SpriteRenderer>().color = new Color(1f, gbValue, gbValue);
+            }
+        }
+    }
+
+}
